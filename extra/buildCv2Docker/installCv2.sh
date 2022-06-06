15832b8fb3e2b7d0a78ad13ba593b9c8444a256a
hugocele <hugo.celedon@nextonia.com.co>
2022-06-06 13:06:59 -0500

    fix: :ambulance: Added -y condition so it does not stop while installing

--- extra/buildCv2Docker/installCv2.sh
+++ extra/buildCv2Docker/installCv2.sh
@@ -1,29 +1,29 @@
 sudo apt update
 sudo apt upgrade
 
-sudo apt install build-essential cmake pkg-config unzip yasm git checkinstall
-sudo apt install libjpeg-dev libpng-dev libtiff-dev
-sudo apt install libavcodec-dev libavformat-dev libswscale-dev libavresample-dev
-sudo apt install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
-sudo apt install libxvidcore-dev x264 libx264-dev libfaac-dev libmp3lame-dev libtheora-dev 
-sudo apt install libfaac-dev libmp3lame-dev libvorbis-dev
-sudo apt install libopencore-amrnb-dev libopencore-amrwb-dev
-sudo apt-get install libdc1394-22 libdc1394-22-dev libxine2-dev libv4l-dev v4l-utils
+sudo apt install -y build-essential cmake pkg-config unzip yasm git checkinstall
+sudo apt install -y libjpeg-dev libpng-dev libtiff-dev
+sudo apt install -y libavcodec-dev libavformat-dev libswscale-dev libavresample-dev
+sudo apt install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+sudo apt install -y libxvidcore-dev x264 libx264-dev libfaac-dev libmp3lame-dev libtheora-dev 
+sudo apt install -y libfaac-dev libmp3lame-dev libvorbis-dev
+sudo apt install -y libopencore-amrnb-dev libopencore-amrwb-dev
+sudo apt-get install -y libdc1394-22 libdc1394-22-dev libxine2-dev libv4l-dev v4l-utils
 cd /usr/include/linux
 sudo ln -s -f ../libv4l1-videodev.h videodev.h
 cd ~/src
 
-sudo apt-get install libgtk-3-dev
-sudo apt-get install python3-dev python3-pip
-sudo -H pip3 install -U pip numpy
+sudo apt-get install -y libgtk-3-dev
+sudo apt-get install -y python3-dev python3-pip
+sudo -H pip3 install -y -U pip numpy
 
-sudo apt install python3-testresources
-sudo apt-get install libtbb-dev
-sudo apt-get install libatlas-base-dev gfortran
-sudo apt-get install libprotobuf-dev protobuf-compiler
-sudo apt-get install libgoogle-glog-dev libgflags-dev
-sudo apt-get install libgphoto2-dev libeigen3-dev libhdf5-dev doxygen
-python3 -m pip install numpy
+sudo apt install -y python3-testresources
+sudo apt-get install -y libtbb-dev
+sudo apt-get install -y libatlas-base-dev gfortran
+sudo apt-get install -y libprotobuf-dev protobuf-compiler
+sudo apt-get install -y libgoogle-glog-dev libgflags-dev
+sudo apt-get install -y libgphoto2-dev libeigen3-dev libhdf5-dev doxygen
+python3 -m pip install -y numpy
 
 cd ~/src
 wget -O opencv.zip https://github.com/opencv/opencv/archive/4.2.0.zip
