46efc42eb4dfe76d4ec675eb0dd57dc0910195da
hugocele <hugo.celedon@nextonia.com.co>
2022-06-06 09:31:01 -0500

    feat: :fire: Created Angular signal and error subplot

--- robotics/ros2/src/plotter/plotter/plotter_node.py
+++ robotics/ros2/src/plotter/plotter/plotter_node.py
@@ -60,15 +60,13 @@ class Plotter(Node):
 
         # Angular
 
-        # ---------------------------------------------------------------------
-        # TODO: Initialize the second subplot
-        # Take care about the variables.
-        # NOT define new variables use the variables defined along the code
-        #
-        # Self-contained reference :smile:
-        #
-        # End Code
-        # ---------------------------------------------------------------------
+        (self.control_ang_ln,) = self.ax[1].plot(
+            [], [], "r", label="Control Angular Signal"
+        )
+        (self.error_angular_ln,) = self.ax[1].plot([], [], "b", label="Angular Error")
+        self.controller_ang_lns = [self.control_ang_ln, self.error_angular_ln]
+        self.ax[1].legend()
+        self.x_linear_data, self.y_linear_data = [[], []], [[], []]
 
         # =============================================================================
         # ROS2 Stuffs
@@ -194,11 +192,10 @@ def main(args=None) -> None:
     plotter_node = Plotter()
 
     # ---------------------------------------------------------------------
-    # TODO: Create a Thread for spin the node
-    # Use the function spin_node
+    # TThread for spin the node
     # https://realpython.com/intro-to-python-threading/
-    spin_node_thread = threading.Thread(target = plotter_node.spin_node)
-    spin_node_thread.setDaemon(True) # So it stops with main thread
+    spin_node_thread = threading.Thread(target = plotter_node.spin_node) # declared Thread
+    spin_node_thread.setDaemon(True) # Set Daemon thread so it stops with main thread does
     spin_node_thread.start() # started thread
     # End Code
     # ---------------------------------------------------------------------
